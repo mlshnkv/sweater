51e6767a5a9b39d3a842cd352305966479a6cc4b
molos <moloshnikov.v@gmail.com>
2020-10-04 12:06:42 +0300

    Init

---
+++ src/main/java/org/moloshnikov/sweater/ServingWebContentApplication.java
@@ -0,0 +1,13 @@
+package org.moloshnikov.sweater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+
+}
